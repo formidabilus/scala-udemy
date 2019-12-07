88465c7adf3483ff1c94e8825af5e0f3cf2a2da5
RazvanChiriac <razvanchiriac@roser-ict.com>
2019-12-07 23:16:48 +0200

    working on collections

---
+++ src/main/scala/rockthejvm/lectures/part3fp/MapFlatmapFilterFor.scala
@@ -0,0 +1,61 @@
+package rockthejvm.lectures.part3fp
+
+object MapFlatmapFilterFor extends App {
+
+  val list = List(1,2,3)
+  println(list.head)
+  println(list.tail)
+
+  // map
+  println(list.map(_ + 1))
+  println(list.map(_ + " is a number"))
+
+  // filter
+  println(list.filter(_ % 2 == 0))
+
+  //flatMap
+  val toPair = (x: Int) => List(x, x+1)
+  println("flatMap: "+ list.flatMap(toPair))
+
+  // print all combinations between two lists
+  val numbers = List(1,2,3,4)
+  val chars = List('a', 'b', 'c', 'd')
+  val colors = List("black", "white")
+  // List("a1", "a2"..."d4")
+
+  val combinations = numbers.filter(_ % 2 == 0). flatMap(n => chars.flatMap(c => colors.map(color => "" + c + n + "-" + color)))
+  val anotherCombination = colors.flatMap(colors => chars.flatMap(c => numbers.map(n => "" + colors + "-" + c + n)))
+  println(combinations)
+  println(anotherCombination)
+
+  // foreach
+  list.foreach(println)
+
+  // for-comprehensions
+  val forComprehensions = for {
+    n <- numbers if n % 2 == 0
+    c <- chars
+    color <- colors
+  } yield "" + c + n + "-" + color
+  println(forComprehensions)
+
+  for {
+    n <- numbers
+  } println(n)
+
+  // syntax overload
+  list.map { x =>
+    x * 2
+  }
+
+  /*
+    1. MyList supports for comprehensions?
+    map(f: A => B) => MyList[B]
+    filter(p: A => Boolean) => MyList[A]
+    flatMap(f: A => MyList[B]) => MyList[B]
+    2. A small collection of at most ONE element - Maybe[+T]
+       - map, flatMap, filter
+   */
+
+  
+}
